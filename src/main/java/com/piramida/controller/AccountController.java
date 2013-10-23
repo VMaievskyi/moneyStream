28025043823b118a828c4e9633677791fde19fca
Slava <maievskyivyacheslav89@gmail.com>
2013-10-23 14:43:32 +0300

    json output added, broken mailing, broken mail.config

--- src/main/java/com/piramida/controller/AccountController.java
+++ src/main/java/com/piramida/controller/AccountController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.piramida.controller.exception.AccountOperationException;
 import com.piramida.entity.Account;
@@ -25,10 +26,21 @@ public class AccountController {
 
     }
 
-    @RequestMapping(value = "/new", method = RequestMethod.POST)
-    public void createAccount(@RequestBody final Account account)
+    @RequestMapping(value = "/new", method = RequestMethod.POST, consumes = "application/json")
+    public Account createAccount(@RequestBody final Account account)
 	    throws AccountOperationException {
 
+	return account;
+
+    }
+
+    @RequestMapping(value = "/echo", method = RequestMethod.GET)
+    @ResponseBody
+    public Account echoService() {
+	final Account a = new Account();
+	a.setEmail("fbsdjbfsdjbf");
+	a.setPassword("dfsknsdjf");
+	return a;
     }
 
     public AccountFacade getAccountFacade() {
