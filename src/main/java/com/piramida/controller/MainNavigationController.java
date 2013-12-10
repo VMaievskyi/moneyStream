e2baf8c98dc627ebb0724a778d8b6d026b596925
slava <maievskyivyacheslav89@gmail.com>
2013-12-10 14:50:15 +0200

    added login page

--- src/main/java/com/piramida/controller/MainNavigationController.java
+++ src/main/java/com/piramida/controller/MainNavigationController.java
@@ -1,6 +1,7 @@
 package com.piramida.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,18 @@ public class MainNavigationController {
     @RequestMapping("index")
     public String index() {
 	return "index";
+    }
+
+    // Login form
+    @RequestMapping("/login")
+    public String login() {
+	return "loginPage";
+    }
+
+    // Login form with error
+    @RequestMapping("/login-error")
+    public String loginError(final Model model) {
+	model.addAttribute("loginError", true);
+	return "loginPage";
     }
 }
